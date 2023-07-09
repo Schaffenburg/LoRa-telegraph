3062389f1b6faa5073dc2e2d4139da3f4542a2c6
Andreas Frisch <github@fraxinas.dev>
2023-07-09 22:37:59 +0200

    receiver: Auto-insert interval between single characters after 1500 ms

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -4,6 +4,7 @@
 #include "heltec.h"
 
 #define BAND    868E6  //you can set band here directly,e.g. 868E6,915E6
+#define SPACE_TIMER_MS 1500
 
 SoftwareSerial SoftSerial(36, 17, false);
 
@@ -34,26 +35,38 @@ void setup() {
 void loop() {
   handleWebClient();
 
+  static unsigned long lastPacketTime = 0; // remembers when the last packet was received
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     String buf;
+    unsigned long currentTime = millis();
+    unsigned long interval = currentTime - lastPacketTime;
 
     // read packet
     while (LoRa.available()) {
       buf.concat(String((char)LoRa.read()));
-
-      Heltec.display->clear();
-      Heltec.display->drawString(0 , 0 , buf);
-      Heltec.display->display();
     }
 
-    Serial.printf("^-. rec '%s'\n", buf);
+    Heltec.display->clear();
+    Heltec.display->drawString(0 , 0 , buf);
 
+    Serial.printf("^-. rec '%s' time passed %lu\n", buf.c_str(), interval);
 
+    // if the packet is a single character, auto-insert space after interval
+    if (buf.length() == 1) {
+
+      if (interval > SPACE_TIMER_MS) {
+        SoftSerial.print(' ');
+        Heltec.display->drawString(0 , 0 , " ");
+      }
+      lastPacketTime = currentTime;
+    }
+
+    Heltec.display->display();
     SoftSerial.print(buf); // write string
   }
-  printLocalTime();
-  delay(1000);
-}
 
+  // printLocalTime();
+  // delay(1000);
+}
 
